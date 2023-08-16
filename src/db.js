60d2a0ad851b510260e99af69dcfe0d43c36dbeb
Patrick <patcerutti23@gmail.com>
2023-08-15 21:46:14 -0500

    Initial commit Prisma database and Nextjs server working

---
+++ src/db.js
@@ -0,0 +1,16 @@
+// to avoid nextjs trying to create many different Prisma Clients
+
+import { PrismaClient } from "@prisma/client";
+
+let prisma;
+
+if (process.env.NODE_ENV === "production") {
+  prisma = new PrismaClient();
+} else {
+  if (!global.prisma) {
+    global.prisma = new PrismaClient();
+  }
+  prisma = global.prisma;
+}
+
+export default prisma;
